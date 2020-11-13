9c0304a4a0ffabb7571aa7e4b5e5a86ce4306022
ylm3112370004 <wintersweety@126.com>
2020-11-13 17:55:54 +0800

    x的平方根

--- week04/69.x的平方根.js
+++ week04/69.x的平方根.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-
+  if (x <= 1) return x;
+  let l = 0, r = x, ans = -1;
+  while (l <= r) {
+    let mid = (l + r) >> 1;
+    if (mid * mid <= x) {
+      ans = mid;
+      l = mid + 1;
+    } else {
+      r = mid -1;
+    }
+  }
+  return ans;
 };
+
+
+// x 的平方根 无限逼近
+// 迭代还可以帮助我们进行无穷次地逼近，求得方程的精确或者近似解。
